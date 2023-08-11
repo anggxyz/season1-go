3ce82a79d57c461512159a0fc35ce7a047e529dd
Angela Gilhotra <angelagilhotra@gmail.com>
2023-08-10 22:17:10 -0700

    switch to goerli

--- app/src/utils/onChainConfig.ts
+++ app/src/utils/onChainConfig.ts
@@ -2,7 +2,7 @@ import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { configureChains } from "wagmi";
 import { foundry, goerli } from "wagmi/chains";
-export const CHAINS = [foundry]
+export const CHAINS = [goerli]
 export const { publicClient, webSocketPublicClient } = configureChains(
   CHAINS,
   // /env.mjs ensures the the app isn't built without .env vars
